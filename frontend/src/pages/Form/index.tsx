4c4d92d1a396eb6dbc2b65948eacf2f1f9247d8c
viviane <vivianesilvarc@outlook.com>
2022-03-12 09:17:49 -0300

    Forms Layouts

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,41 @@
-function Form(){
-    return(
-        <h1>Pagina de formulario</h1>
+import './styles.css'
+
+function Form() {
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rqeYMLryjcawh2JeRpCVUDXYM5b.jpg",
+        title: "The Walking Dead",
+        count: 2,
+        score: 4.5
+    };
+    return (
+        <div className="dsmovie-form-container">
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="dsmovie-form">
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="dsmovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+            </div >
+        </div >
     );
 }
 export default Form;
